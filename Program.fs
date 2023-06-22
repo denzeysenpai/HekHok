b202bd460463d135c57e6bc0be772a8fc5c8e82e
Sascha <72701348+denzeysenpai@users.noreply.github.com>
2023-06-22 11:16:46 +0800

    2

--- Program.fs
+++ Program.fs
@@ -1,9 +1,8 @@
-﻿open System.Threading
+open System.Threading
 open System
 
 let mutable monologue = string("So you need help?")
 let mutable count = 0
-
 
 for i in monologue do
     stdout.Write(monologue[count])
@@ -46,7 +45,7 @@ for i in monologue do
     count <- count + 1
     Thread.Sleep(50) //Async.Sleep(50) |> Async.RunSynchronously
 Console.Clear()
-monologue <- "just wait for a second..."
+monologue <- "...just wait for a second..."
 count <- 0
 for i in monologue do
     stdout.Write(monologue[count])
@@ -65,7 +64,7 @@ count <- 0
 for i in monologue do
     stdout.Write(monologue[count])
     count <- count + 1
-    Thread.Sleep(50) //Async.Sleep(50) |> Async.RunSynchronously
+    Thread.Sleep(60) //Async.Sleep(50) |> Async.RunSynchronously
 Console.Clear()
 monologue <- "This program is designed for the sole purpose of managing the system that it was designed along with blah blah blah"
 count <- 0
@@ -79,6 +78,37 @@ count <- 0
 for i in monologue do
     stdout.Write(monologue[count])
     count <- count + 1
+    Thread.Sleep(65) //Async.Sleep(50) |> Async.RunSynchronously
+Console.Write(" Press enter key to continue...")
+Console.ReadKey() |> ignore //--------------------------------------------------------//
+Console.Clear()
+monologue <- "Please select from the given choices of whatever it is you want to do..."
+count <- 0
+for i in monologue do
+    stdout.Write(monologue[count])
+    count <- count + 1
     Thread.Sleep(50) //Async.Sleep(50) |> Async.RunSynchronously
+Console.WriteLine("\n\n\t\tChange System password  [0]")
+Console.WriteLine("\n\t\tBug Report\t\t[1]")
+Console.WriteLine("\n\t\tSystem Performance Test [2]")
+Console.WriteLine("\n\t\tSystem Reformat\t\t[3]")
+Console.WriteLine("\n\t\tSystem Checkup\t\t[4]")
+Console.WriteLine("\n\t\tDatabase\t\t[5]")
+monologue <- "Choice: "
+count <- 0
+for i in monologue do
+    stdout.Write(monologue[count])
+    count <- count + 1
+    Thread.Sleep(50) //Async.Sleep(50) |> Async.RunSynchronously
+Console.ForegroundColor <- ConsoleColor.Green 
+
+let mutable choice = 
+    try
+        int(Console.ReadLine())
+    with
+        | :? System.FormatException -> 99;
+if choice.Equals(int(99))
+then printfn "That is not in the given choices!"
+else printfn "Please Wait"
+
 Console.ReadKey() |> ignore
-//
